cc1d307e1429a0ab8938b772e4edf2ad8d6346ae
AuroraVaughn <vaughn.aurora@gmail.com>
2024-02-19 13:43:57 -0600

    extend variables object type

---
+++ src/scripts/state/state-variables.types.d.ts
@@ -0,0 +1,12 @@
+/**
+This is a dummy interface for the story variables object. Its sole purpose is to be merged
+ * with user declarations that provide its real game content.
+ */
+declare module "twine-sugarcube" {
+  interface SugarCubeStoryVariables {
+    AddNewVariablesHere: string;
+  }
+}
+ 
+export { };
+ 
